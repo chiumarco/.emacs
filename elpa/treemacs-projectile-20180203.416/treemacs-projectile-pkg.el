e934f76ce491b704ade54251cea220ec61fb8762
Marco Chiu <chiumarco@gmail.com>
2018-05-08 01:15:51 +0800

    Updated on 6 May am.

--- elpa/treemacs-projectile-20180203.416/treemacs-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-projectile" "20180203.416" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0")) :commit "e8be02971ad6c7a9c33ec544fa41b4b2f6827a35" :url "https://github.com/Alexander-Miller/treemacs")
