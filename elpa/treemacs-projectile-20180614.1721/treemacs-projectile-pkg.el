57033d08dbafff350e7e6f88a4c78560e83421b4
Marco Chiu <chiumarco@gmail.com>
2018-08-29 01:17:47 +0800

    Update on 29 Aug AM.

--- elpa/treemacs-projectile-20180614.1721/treemacs-projectile-pkg.el
+++ elpa/treemacs-projectile-20180614.1721/treemacs-projectile-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-projectile" "20180614.1721" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0")) :commit "7b831e9d86cb8c964ebae10502823478c0c51d26" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
+(define-package "treemacs-projectile" "20180614.1721" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0")) :commit "de67d8b0412acd8a199584c335802fec2e330fa8" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
