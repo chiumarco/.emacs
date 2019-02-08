b67ba56378e58c8276e5d8a9301cb44cf0912861
Marco Chiu <chiumarco@gmail.com>
2019-02-08 21:17:20 +0800

    Update on Feb 8.

---
+++ elpa/mmt-20190101.707/mmt-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mmt" "20190101.707" "Missing macro tools for Emacs Lisp" '((emacs "24.1") (cl-lib "0.3")) :commit "db0f27b10bba0b26cdd208e9f6467bf455021e48" :keywords '("macro" "emacs-lisp") :authors '(("Mark Karpov" . "markkarpov92@gmail.com")) :maintainer '("Mark Karpov" . "markkarpov92@gmail.com") :url "https://github.com/mrkkrp/mmt")
