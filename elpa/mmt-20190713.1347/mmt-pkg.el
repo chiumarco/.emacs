b8fc0e93efb7fb660d04fd4e7cdb8fce92ecafb9
Marco Chiu <chiumarco@gmail.com>
2019-07-29 07:56:46 +0800

    update on 29 Jul.

---
+++ elpa/mmt-20190713.1347/mmt-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mmt" "20190713.1347" "Missing macro tools for Emacs Lisp" '((emacs "24.1") (cl-lib "0.3")) :commit "753f6dc888acbd932c4fbd7c73ff750381058561" :keywords '("macro" "emacs-lisp") :authors '(("Mark Karpov" . "markkarpov92@gmail.com")) :maintainer '("Mark Karpov" . "markkarpov92@gmail.com") :url "https://github.com/mrkkrp/mmt")
--- elpa/treemacs-projectile-20190114.1634/treemacs-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-projectile" "20190114.1634" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0.0")) :commit "c3b52086068f7ca838f31e51724498eada26d354" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
